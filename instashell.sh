e319bb175952ed06f15702f99dac427a93276f8c
toxictech1 <93738730+toxictech1@users.noreply.github.com>
2023-11-11 00:25:03 +0100

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Coded by @thelinuxchoice (Instagram)
-checkroot() {
+checkroot() 
 if [[ "$(id -u)" -ne 0 ]]; then
     printf "\e[1;77mPlease, run this program as root!\n\e[0m"
     exit 1
